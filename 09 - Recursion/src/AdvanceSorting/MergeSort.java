461d8ebcae14cdf5702f4e7dd46a2a2626b75e3c
bhaveshkundwani <kundwanibhavesh@gmail.com>
2025-11-09 19:19:43 +0530

    Recursion

--- 09 - Recursion/src/AdvanceSorting/MergeSort.java
+++ 09 - Recursion/src/AdvanceSorting/MergeSort.java
@@ -1,10 +1,20 @@
 package AdvanceSorting;
 
+import java.util.Arrays;
+
 public class MergeSort {
     public static void main(String[] args) {
         int[] arr = {5, 2, 8, 4, 1, 6, 7, 3, 9};
+//        print(arr);
+//        mergeSort(arr);
+//        print(arr);
+
+//        print(arr);
+//        arr = mergeSort(arr);
+//        print(arr);
+
         print(arr);
-        mergeSort(arr);
+        mergeSortInPlace(arr, 0, arr.length);
         print(arr);
     }
 
@@ -15,52 +25,150 @@ public class MergeSort {
         System.out.println();
     }
 
-    public static void mergeSort(int[] arr) {
-        int n = arr.length;
-        if (n == 1) return;   // 1 length array is sorted already
+//    public static void mergeSort(int[] arr) {
+//        int n = arr.length;
+//        if (n == 1) return;   // 1 length array is sorted already
+//
+//        // Step 1 : Create 2 new Empty arrays of size n / 2 each
+//        int[] a = new int[n / 2];
+//        int[] b = new int[n - n / 2];
+//
+//        // Step 2 : Copy-paste arr into a and b
+//        int idx = 0; // idx travel on arr
+//        for (int i = 0; i < a.length; i++){
+//            a[i] = arr[idx++];
+//        }
+//        for (int i = 0; i < b.length; i++){
+//            b[i] = arr[idx++];
+//        }
+//
+//        // Step 3 : Apply recursion on a and b
+//        mergeSort(a);
+//        mergeSort(b);
+//
+//        // Step 4 : Merge a and b into arr
+//        merge(a, b, arr);
+//    }
+//
+//    public static void merge(int[] a, int[] b, int[] arr) {
+//        int i = 0, j = 0, k = 0;
+//
+//        while (i < a.length && j < b.length) {
+//            if (a[i] <= b[j]) {
+//                arr[k] = a[i];
+//                i++; k++;
+//            }
+//            else {
+//                arr[k] = b[j];
+//                j++; k++;
+//            }
+//        }
+//
+//        while (i < a.length) {
+//            arr[k] = a[i];
+//            i++; k++;
+//        }
+//        while (j < b.length) {
+//            arr[k] = b[j];
+//            j++; k++;
+//        }
+//    }
 
-        // Step 1 : Create 2 new Empty arrays of size n / 2 each
-        int[] a = new int[n / 2];
-        int[] b = new int[n - n / 2];
-
-        // Step 2 : Copy-paste arr into a and b
-        int idx = 0; // idx travel on arr
-        for (int i = 0; i < a.length; i++){
-            a[i] = arr[idx++];
+    public static int[] mergeSort(int[] arr) {
+        if (arr.length == 1) {
+            return arr;
         }
-        for (int i = 0; i < b.length; i++){
-            b[i] = arr[idx++];
-        }
 
-        // Step 3 : Apply recursion on a and b
-        mergeSort(a);
-        mergeSort(b);
+        int mid = arr.length / 2;
 
-        // Step 4 : Merge a and b into arr
-        merge(a, b, arr);
+        int[] left = mergeSort(Arrays.copyOfRange(arr, 0, mid));
+        int[] right = mergeSort(Arrays.copyOfRange(arr, mid, arr.length));
+
+        return merge(left, right);
     }
 
-    public static void merge(int[] a, int[] b, int[] arr) {
-        int i = 0, j = 0, k = 0;
+    public static int[] merge(int[] first, int[] second) {
+        int[] mix = new int[first.length + second.length];
 
-        while (i < a.length && j < b.length) {
-            if (a[i] <= b[j]) {
-                arr[k] = a[i];
-                i++; k++;
+        int i = 0;
+        int j = 0;
+        int k = 0;
+
+        while (i < first.length && j < second.length) {
+            if (first[i] < second[j]) {
+                mix[k] = first[i];
+                i++;
+            } else {
+                mix[k] = second[j];
+                j++;
             }
-            else {
-                arr[k] = b[j];
-                j++; k++;
-            }
+            k++;
         }
 
-        while (i < a.length) {
-            arr[k] = a[i];
-            i++; k++;
+        // it may be possible that one of the arrays is not complete
+        // copy the remaining elements
+        while (i < first.length) {
+            mix[k] = first[i];
+            i++;
+            k++;
         }
-        while (j < b.length) {
-            arr[k] = b[j];
-            j++; k++;
+
+        while (j < second.length) {
+            mix[k] = second[j];
+            j++;
+            k++;
+        }
+
+        return mix;
+    }
+
+    public static void mergeSortInPlace(int[] arr, int s, int e) {
+        if (e - s == 1) {
+            return;
+        }
+
+        int mid = (s + e) / 2;
+
+        mergeSortInPlace(arr, s, mid);
+        mergeSortInPlace(arr, mid, e);
+
+        mergeInPlace(arr, s, mid, e);
+    }
+
+    public static void mergeInPlace(int[] arr, int s, int m, int e) {
+        int[] mix = new int[e - s];
+
+        int i = s;
+        int j = m;
+        int k = 0;
+
+        while (i < m && j < e) {
+            if (arr[i] < arr[j]) {
+                mix[k] = arr[i];
+                i++;
+            } else {
+                mix[k] = arr[j];
+                j++;
+            }
+            k++;
+        }
+
+        // it may be possible that one of the arrays is not complete
+        // copy the remaining elements
+        while (i < m) {
+            mix[k] = arr[i];
+            i++;
+            k++;
+        }
+
+        while (j < e) {
+            mix[k] = arr[j];
+            j++;
+            k++;
+        }
+
+        for (int l = 0; l < mix.length; l++) {
+            arr[s+l] = mix[l];
         }
     }
 }
--- 09 - Recursion/src/AdvanceSorting/QuickSort.java
+++ 09 - Recursion/src/AdvanceSorting/QuickSort.java
@@ -2,6 +2,101 @@ package AdvanceSorting;
 
 public class QuickSort {
     public static void main(String[] args) {
+        int[] arr = {10, 7, 8, 9, 1, 5};
+        int n = arr.length;
 
+        print(arr);
+//        quickSort(arr, 0, n - 1);
+        sort(arr, 0, arr.length - 1);
+        print(arr);
+    }
+
+    public static void print(int[] arr) {
+        for (int ele : arr) {
+            System.out.print(ele +  " ");
+        }
+        System.out.println();
+    }
+
+    // the QuickSort function implementation
+    public static void quickSort(int[] arr, int low, int high) {
+        if (low < high) {
+
+            // pi is the partition return index of pivot
+            int pi = partition(arr, low, high);
+
+            // recursion calls for smaller elements
+            // and greater or equals elements
+            quickSort(arr, low, pi - 1);
+            quickSort(arr, pi + 1, high);
+        }
+    }
+
+    // partition function
+    public static int partition(int[] arr, int low, int high) {
+
+        // choose the pivot
+        int pivot = arr[high];
+
+        // index of smaller element and indicates
+        // the right position of pivot found so far
+        int i = low - 1;
+
+        // traverse arr[low..high] and move all smaller
+        // elements to the left side. Elements from low to
+        // i are smaller after every iteration
+        for (int j = low; j <= high - 1; j++) {
+            if (arr[j] < pivot) {
+                i++;
+                swap(arr, i, j);
+            }
+        }
+
+        // Move pivot after smaller elements and
+        // return its position
+        swap(arr, i + 1, high);
+        return i + 1;
+    }
+
+    // swap function
+    public static void swap(int[] arr, int i, int j) {
+        int temp = arr[i];
+        arr[i] = arr[j];
+        arr[j] = temp;
+    }
+
+    // another quick sort, taking mid as pivot
+    public static void sort(int[] nums, int low, int hi) {
+        if (low >= hi) {
+            return;
+        }
+
+        int s = low;
+        int e = hi;
+        int m = s + (e - s) / 2;
+        int pivot = nums[m];
+
+        while (s <= e) {
+
+            // also a reason why if its already sorted it will not swap
+            while (nums[s] < pivot) {
+                s++;
+            }
+            while (nums[e] > pivot) {
+                e--;
+            }
+
+            if (s <= e) {
+                int temp = nums[s];
+                nums[s] = nums[e];
+                nums[e] = temp;
+                s++;
+                e--;
+            }
+        }
+
+        // now my pivot is at correct index, please sort two halves now
+        sort(nums, low, e);
+        sort(nums, s, hi);
     }
 }
