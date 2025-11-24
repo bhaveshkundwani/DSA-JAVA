f6ae8d83d94aebccaabcb3812f0d95ff2fa36f06
bhaveshkundwani <kundwanibhavesh@gmail.com>
2025-11-24 15:28:52 +0530

    Linked List

--- 11 - Linked List/src/com/Questions.java
+++ 11 - Linked List/src/com/Questions.java
@@ -37,6 +37,141 @@ public class Questions {
         }
         return 0;
     }
+
+    // detect cycle and return start index of cycle
+    public ListNode detectCycle(ListNode head) {
+        int length = 0;
+
+        ListNode fast = head;
+        ListNode slow = head;
+
+        while (fast != null && fast.next != null) {
+            fast = fast.next.next;
+            slow = slow.next;
+            if (fast == slow) {
+                length = lengthCycle(slow);
+                break;
+            }
+        }
+
+        if (length == 0) {
+            return null;
+        }
+
+        // find the start node
+        ListNode first = head;
+        ListNode second = head;
+
+        while (length > 0) {
+            second = second.next;
+            length--;
+        }
+
+        // keep moving both forward and they will meet at cycle start
+        while (first != second) {
+            first = first.next;
+            second = second.next;
+        }
+
+        return second;
+    }
+
+    // happy number
+    public boolean isHappy(int n) {
+        int slow = n;
+        int fast = n;
+
+        do {
+            slow = findSquare(slow);
+            fast = findSquare(findSquare(fast));
+        } while (slow != fast);
+
+        if (slow == 1) {
+            return true;
+        }
+        return false;
+    }
+
+    private int findSquare(int number) {
+        int ans = 0;
+        while (number > 0) {
+            int rem = number % 10 ;
+            ans += rem * rem;
+            number /= 10;
+        }
+        return ans;
+    }
+
+    // find middle node
+    public ListNode middleNode(ListNode head) {
+        ListNode slow = head;
+        ListNode fast = head;
+
+        while (fast != null && fast.next != null) {
+            slow = slow.next;
+            fast = fast.next.next;
+        }
+
+        return slow;
+    }
+
+    // google, apple, amazon, microsoft
+    public ListNode reverseList(ListNode head) {
+        if (head == null) {
+            return head;
+        }
+        ListNode prev = null;
+        ListNode present = head;
+        ListNode next = present.next;
+
+        while (present != null) {
+            present.next = prev;
+            prev = present;
+            present = next;
+            if (next != null) {
+                next = next.next;
+            }
+        }
+        return prev;
+    }
+
+    // Google, Microsoft, Facebook
+    public ListNode reverseBetween(ListNode head, int left, int right) {
+        if (left == right) {
+            return head;
+        }
+
+        // skip the first left-1 nodes
+        ListNode curr = head;
+        ListNode prev = null;
+        for (int i = 0; curr != null && i < left - 1; i++) {
+            prev = curr;
+            curr = curr.next;
+        }
+
+        ListNode last = prev;
+        ListNode newEnd = curr;
+
+        // reverse between left and right
+        ListNode next = curr.next;
+        for (int i = 0; curr != null && i < right - left + 1; i++) {
+            curr.next = prev;
+            prev = curr;
+            curr = next;
+            if (next != null) {
+                next = next.next;
+            }
+        }
+
+        if (last != null) {
+            last.next = prev;
+        } else {
+            head = prev;
+        }
+
+        newEnd.next = curr;
+        return head;
+    }
 }
 
 class ListNode {
